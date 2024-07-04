d437f4f1ab9b48cf500c7df8850d0ea5fc38146d
LamNguyen176 <devlamnt176@gmail.com>
2024-07-04 10:50:43 +0700

    add firebase(#development)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'package:auth_flow_flutter_rxdart/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

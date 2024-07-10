8ce5ee85adc3dd19fecd9a4c43e0287d39a5678a
Forest Nguyen <devlamnt176@gmail.com>
2024-07-10 23:15:23 +0700

    disable back(#development)

--- lib/presentation/features/auth/user_info/user_info_screen.dart
+++ lib/presentation/features/auth/user_info/user_info_screen.dart
@@ -25,96 +25,98 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: const Text('User Info'),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 16.0,
-            right: 16.0,
-            bottom: 20.0,
+    return PopScope(
+        canPop: true,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: const Text('User Info'),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Row(),
-              _user.photoURL != null
-                  ? ClipOval(
-                      child: Material(
-                        child: Image.network(
-                          _user.photoURL!,
-                          fit: BoxFit.fitHeight,
-                        ),
-                      ),
-                    )
-                  : const ClipOval(
-                      child: Material(
-                        child: Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Icon(
-                            Icons.person,
-                            size: 60,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 16.0,
+                right: 16.0,
+                bottom: 20.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Row(),
+                  _user.photoURL != null
+                      ? ClipOval(
+                          child: Material(
+                            child: Image.network(
+                              _user.photoURL!,
+                              fit: BoxFit.fitHeight,
+                            ),
+                          ),
+                        )
+                      : const ClipOval(
+                          child: Material(
+                            child: Padding(
+                              padding: EdgeInsets.all(16.0),
+                              child: Icon(
+                                Icons.person,
+                                size: 60,
+                              ),
+                            ),
                           ),
                         ),
+                  const SizedBox(height: 16.0),
+                  const Text(
+                    'Hello',
+                    style: TextStyle(
+                      fontSize: 26,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    _user.displayName ?? '',
+                    style: const TextStyle(
+                      fontSize: 26,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    '( ${_user.email ?? ''} )',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                  const SizedBox(height: 24.0),
+                  const Text(
+                    'You are now signed in using your Google account. To sign out of your account, click the "Sign Out" button below.',
+                    style: TextStyle(fontSize: 14, letterSpacing: 0.2),
+                  ),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      _authBloc.logout.add(null);
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.redAccent,
+                      ),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                     ),
-              const SizedBox(height: 16.0),
-              const Text(
-                'Hello',
-                style: TextStyle(
-                  fontSize: 26,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                _user.displayName ?? '',
-                style: const TextStyle(
-                  fontSize: 26,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                '( ${_user.email ?? ''} )',
-                style: const TextStyle(
-                  fontSize: 20,
-                  letterSpacing: 0.5,
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              const Text(
-                'You are now signed in using your Google account. To sign out of your account, click the "Sign Out" button below.',
-                style: TextStyle(fontSize: 14, letterSpacing: 0.2),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  _authBloc.logout.add(null);
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                    Colors.redAccent,
+                    child: const Text('Đăng xuất',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          letterSpacing: 2,
+                        )),
                   ),
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ),
-                child: const Text('Đăng xuất',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      letterSpacing: 2,
-                    )),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
